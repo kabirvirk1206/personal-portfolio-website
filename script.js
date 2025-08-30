94411c743c800523aafa668415f952149b76e0ef
Kabir Virk <203725957+kabirvirk1206@users.noreply.github.com>
2025-08-30 15:12:52 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function copyEmail() {
+  //in later renditions of this website, add much more js functionality aswell as usage of different frameworks
+  navigator.clipboard.writeText("kabirvirk1206@gmail.com");
+  const el = document.getElementById("copied");
+  if (!el) return;
+  el.style.opacity = 1;
+  setTimeout(() => (el.style.opacity = 0), 1100);
+}
+
+// set the current year in footer (stays updated)
+document.getElementById("year").textContent = new Date().getFullYear();
